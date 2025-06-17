7775f312408a0ae455a5cdc46498738f3c6fa410
laijj <laijj@asiainfo.com>
2025-06-17 09:27:39 +0800

    @bpascal@修改编译配置
    @bpascal@修改编译配置

--- childroid/app/src/main/java/parentalcontroller/child/GUI/MapViewActivity.java
+++ childroid/app/src/main/java/parentalcontroller/child/GUI/MapViewActivity.java
@@ -1,112 +1,66 @@
 package parentalcontroller.child.GUI;
 
-import java.util.List;
-
-import parentalcontroller.child.R;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Point;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.widget.Toast;
 
-import com.google.android.maps.GeoPoint;
-import com.google.android.maps.MapActivity;
-import com.google.android.maps.MapController;
-import com.google.android.maps.MapView;
-import com.google.android.maps.Overlay;
+import androidx.fragment.app.FragmentActivity;
 
-public class MapViewActivity extends MapActivity {
-	MapView mapView;
-	MapController mc;
-	GeoPoint p;
-	private LocationManager lm;
-	private LocationListener locationListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-	class MapOverlay extends com.google.android.maps.Overlay {
-		@Override
-		public boolean draw(Canvas canvas, MapView mapView, boolean shadow,
-				long when) {
-			super.draw(canvas, mapView, shadow);
-			// ---translate the GeoPoint to screen pixels---
-			Point screenPts = new Point();
-			mapView.getProjection().toPixels(p, screenPts);
-			// ---add the marker---
-			Bitmap bmp = BitmapFactory.decodeResource(getResources(),
-					R.drawable.ic_launcher);
-			canvas.drawBitmap(bmp, screenPts.x, screenPts.y - 50, null);
-			return true;
-		}
+import parentalcontroller.child.R;
 
-		@Override
-		public boolean onTouchEvent(MotionEvent event, MapView mapView) {
-			// ---when user lifts his finger---
-			if (event.getAction() == 1) {
-				GeoPoint p = mapView.getProjection().fromPixels(
-						(int) event.getX(), (int) event.getY());
-				Toast.makeText(
-						getBaseContext(),
-						"Location: " + p.getLatitudeE6() / 1E6 + ","
-								+ p.getLongitudeE6() / 1E6, Toast.LENGTH_SHORT)
-						.show();
-			}
-			return false;
-		}
+public class MapViewActivity extends FragmentActivity implements OnMapReadyCallback {
+	private GoogleMap mMap;
+	private static final LatLng DEFAULT_LOCATION = new LatLng(1.352566007, 103.78921587);
 
-	}
-
-	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mapviewlayout);
 
-		this.mapView = (MapView) findViewById(R.id.mapView);
-		this.mapView.setBuiltInZoomControls(true);
-
-		mapView.setSatellite(true);
-		// mapView.setStreetView(true);
-		// mapView.setTraffic(true);
-
-		mc = mapView.getController();
-		String coordinates[] = { "1.352566007", "103.78921587" };
-		double lat = Double.parseDouble(coordinates[0]);
-		double lng = Double.parseDouble(coordinates[1]);
-		p = new GeoPoint((int) (lat * 1E6), (int) (lng * 1E6));
-		mc.animateTo(p);
-		mc.setZoom(13);
-
-		// ---Add a location marker---
-		MapOverlay mapOverlay = new MapOverlay();
-		List<Overlay> listOfOverlays = mapView.getOverlays();
-		listOfOverlays.clear();
-		listOfOverlays.add(mapOverlay);
-
-		mapView.invalidate();
-
-	}
-
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		MapController mc = mapView.getController();
-		switch (keyCode) {
-		case KeyEvent.KEYCODE_3:
-			mc.zoomIn();
-			break;
-		case KeyEvent.KEYCODE_1:
-			mc.zoomOut();
-			break;
-		}
-		return super.onKeyDown(keyCode, event);
+		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
+		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.mapView);
+		mapFragment.getMapAsync(this);
 	}
 
 	@Override
-	protected boolean isRouteDisplayed() {
-		return false;
+	public void onMapReady(GoogleMap googleMap) {
+		mMap = googleMap;
+
+		// Add a marker at the default location and move the camera
+		mMap.addMarker(new MarkerOptions().position(DEFAULT_LOCATION).title("Marker"));
+		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, 13));
+
+		// Enable satellite view
+		mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+		// Set click listener
+		mMap.setOnMapClickListener(latLng -> {
+			Toast.makeText(
+				getBaseContext(),
+				"Location: " + latLng.latitude + "," + latLng.longitude,
+				Toast.LENGTH_SHORT
+			).show();
+		});
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		switch (keyCode) {
+		case KeyEvent.KEYCODE_3:
+			mMap.animateCamera(CameraUpdateFactory.zoomIn());
+			break;
+		case KeyEvent.KEYCODE_1:
+			mMap.animateCamera(CameraUpdateFactory.zoomOut());
+			break;
+		}
+		return super.onKeyDown(keyCode, event);
 	}
 }
